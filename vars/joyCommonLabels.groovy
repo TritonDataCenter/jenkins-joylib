881335e60f67400af162cd80f5f70029ceb1818d
Chris Burroughs <chris.burroughs@joyent.com>
2019-10-16 17:30:38 -0400

    poc

--- vars/joyCommonLabels.groovy
+++ vars/joyCommonLabels.groovy
@@ -10,13 +10,19 @@
 /**
  
 */
-void call(String img_ver = "18.4.1", String pi = '20151126T062538Z', String jenkins_agent = '2') {
+//void call(String img_ver = "18.4.1", String pi = '20151126T062538Z', String jenkins_agent = '2') {    
+void call(Map args = [:]) {
     // example: '!platform:true && image_ver:18.4.0 && pkgsrc_arch:x86_64 && pi:20151126T062538Z && jenkins_agent:2'
+    args.img_ver = args.img_ver ?: '18.4.1';
+    args.pi = args.pi ?: '20151126T062538Z';
+    args.jenkins_agent = args.jenkins_agent ?: '2':
+    
+    
     String pkgsrc_arch = 'x86_64';
-    if (img_ver < '18.4.0') {
+    if (args.img_ver < '18.4.0') {
         pkgsrc_arch = 'multiarch';
     }
-    String labels = "!platform:true && image_ver:${img_ver} && pkgsrc_arch:${pkgsrc_arch} && pi:${pi} && jenkins_agent:${jenkins_agent}";
+    String labels = "!platform:true && image_ver:${args.img_ver} && pkgsrc_arch:${pkgsrc_arch} && pi:${pi} && jenkins_agent:${args.jenkins_agent}";
     echo "joyent common labels: ${labels}";
     return labels;
 }
