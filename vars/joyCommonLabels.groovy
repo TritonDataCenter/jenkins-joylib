e4f1ea3ccd89f376996fd52d045c2f427db6829b
Chris Burroughs <chris.burroughs@joyent.com>
2019-10-16 17:33:19 -0400

    poc

--- vars/joyCommonLabels.groovy
+++ vars/joyCommonLabels.groovy
@@ -8,16 +8,17 @@
 
 
 /**
- 
+
 */
-//void call(String img_ver = "18.4.1", String pi = '20151126T062538Z', String jenkins_agent = '2') {    
+//void call(String img_ver = "18.4.1", String pi = '20151126T062538Z', String jenkins_agent = '2') {
 void call(Map args = [:]) {
     // example: '!platform:true && image_ver:18.4.0 && pkgsrc_arch:x86_64 && pi:20151126T062538Z && jenkins_agent:2'
-    args.img_ver = args.img_ver ?: '18.4.1';
+    if (! args.image_ver) {
+        throw new Exception("missing required parameter img_ver");
+    }
     args.pi = args.pi ?: '20151126T062538Z';
-    args.jenkins_agent = args.jenkins_agent ?: '2':
-    
-    
+    args.jenkins_agent = args.jenkins_agent ?: '2';
+
     String pkgsrc_arch = 'x86_64';
     if (args.img_ver < '18.4.0') {
         pkgsrc_arch = 'multiarch';
