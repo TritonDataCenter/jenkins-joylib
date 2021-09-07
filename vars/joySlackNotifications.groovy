d809429b0949d8ef6cb4aa953b1bf7fd598f3866
Brian Bennett <brian.bennett@joyent.com>
2021-09-07 10:37:13 -0700

    Switch to slack notifications (#8)

--- vars/joySlackNotifications.groovy
+++ vars/joySlackNotifications.groovy
@@ -3,19 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright 2020 Joyent, Inc.
+ * Copyright 2021 Joyent, Inc.
  */
 
 
 /**
- * Notify the jenkins mattermost channel, only on master/release branches, and
+ * Notify the jenkins Slack channel, only on master/release branches, and
  * don't neglect colors or emoji.
 */
 void call(Map args = [:]) {
     String channel = args.channel ?: 'jenkins';
     String comment = args.comment ?: '';
 
-    // addapted from https://github.com/jenkinsci/mattermost-plugin/blob/mattermost-2.7.0/src/main/java/jenkins/plugins/mattermost/ActiveNotifier.java
+    // adapted from https://github.com/jenkinsci/slack-plugin/blob/slack-2.48/src/main/java/jenkins/plugins/slack/ActiveNotifier.java
     def STATUS_MAP = ['SUCCESS': ':white_check_mark:', 'FAILURE': 'no_entry_sign:',
                       'UNSTABLE': ':no_entry_sign:', 'ABORTED': ':warning:',
                       'NOT_BUILT': ':warning:', 'UNSTABLE': ':warning:'];
@@ -26,32 +26,32 @@ void call(Map args = [:]) {
 
     String branch = env.BRANCH_NAME;
     if (!branch) {
-        echo "[joyMattermostNotification] env.BRANCH_NAME=${env.BRANCH_NAME}, trying to guess real branch...";
+        echo "[joySlackNotification] env.BRANCH_NAME=${env.BRANCH_NAME}, trying to guess real branch...";
         branch = sh(returnStdout: true, script: 'git symbolic-ref HEAD | cut -d / -f 3').trim();
-        echo "[joyMattermostNotification] guessed ${branch}";
+        echo "[joySlackNotification] guessed ${branch}";
     }
 
-    String mmText = "";
+    String slackText = "";
     if (currentBuild.description) {
-        mmText = currentBuild.description;
+        slackText = currentBuild.description;
     } else if (branch) {
-        mmText = branch;
+        slackText = branch;
     }
 
     // Callers can include a comment which is useful to differentiate
     // notifications from different pipeline stages, but can be used for any
     // purpose.
     if (comment) {
-        mmText += ' ' + comment;
+        slackText += ' ' + comment;
     }
 
     if (branch == 'master' || branch == 'mantav1' || branch ==~ '^release.*') {
-        mattermostSend(
+        slackSend(
             channel: channel,
             color: "${if (currentBuild.currentResult == 'SUCCESS') 'good' else 'danger'}",
             message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} ${emoji} ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)",
-            text: mmText)
+            text: slackText)
     } else {
-        echo "[joyMattermostNotification] not in master, mantav1, or release-YYYYMMDD branch: suppressing notification"
+        echo "[joySlackNotification] not in master, mantav1, or release-YYYYMMDD branch: suppressing notification"
     }
 }
